6cdba5254f71dac52f8211be8498f633d5891aee
AnnaVoronova <amoramio@gmail.com>
2024-08-29 01:35:10 -0400

    product validation tests

--- src/ui/pages/products/addNewProduct.page.ts
+++ src/ui/pages/products/addNewProduct.page.ts
@@ -10,6 +10,10 @@ export class AddNewProductPage extends PortalPage {
   private readonly 'Amount input' = '#inputAmount';
   private readonly 'Notes textarea' = '#textareaNotes';
   private readonly 'Save New Product button' = '#save-new-product';
+  private readonly 'Name Notification' = '#error-inputName';
+  private readonly 'Price Notification' = '#error-inputPrice';
+  private readonly 'Amount Notification' = '#error-inputAmount';
+  private readonly 'Notes Notification' = '#error-textareaNotes';
 
   async fillInputs(product: Partial<IProduct>) {
     product.name && (await this.setValue(this['Name input'], product.name));
@@ -17,6 +21,137 @@ export class AddNewProductPage extends PortalPage {
     product.price !== undefined && (await this.setValue(this['Price input'], product.price));
     product.amount !== undefined && (await this.setValue(this['Amount input'], product.amount));
     product.notes && (await this.setValue(this['Notes textarea'], product.notes));
+  }
+
+  async fillInputsValidMinValues(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name.slice(0, 3)));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    await this.setValue(this['Price input'], 1);
+    await this.setValue(this['Amount input'], 0);
+  }
+
+  async fillInputsValidMaxValues(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name.slice(0, 12) + Date.now() + Date.now()));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    await this.setValue(this['Price input'], 99999);
+    await this.setValue(this['Amount input'], 999);
+    await this.setValue(
+      this['Notes textarea'],
+      'Обязательное НЕобязательное Разрешенные символы:1. Любые символы, krome:2. Длина: 0-250 Выбор одного значения из:●	Apple●	Samsung●	Google●	Microsoft●	Sony●	XiaomiAmazonTeslaСтили компонента описаны в разделе “Add New Product Components” документа “Components”',
+    );
+  }
+
+  async fillInputsShortName(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name.slice(0, 1)));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    product.price !== undefined && (await this.setValue(this['Price input'], product.price));
+    product.amount !== undefined && (await this.setValue(this['Amount input'], product.amount));
+    product.notes && (await this.setValue(this['Notes textarea'], product.notes));
+  }
+
+  async fillInputsLongName(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name + 'soo many symbols - qwertyuiopasdfghjklzx'));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    product.price !== undefined && (await this.setValue(this['Price input'], product.price));
+    product.amount !== undefined && (await this.setValue(this['Amount input'], product.amount));
+    product.notes && (await this.setValue(this['Notes textarea'], product.notes));
+  }
+
+  async fillInputs2SpacesName(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name + 't  o o many spaces'));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    product.price !== undefined && (await this.setValue(this['Price input'], product.price));
+    product.amount !== undefined && (await this.setValue(this['Amount input'], product.amount));
+    product.notes && (await this.setValue(this['Notes textarea'], product.notes));
+  }
+
+  async fillInputsZeroPrice(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    await this.setValue(this['Price input'], 0);
+    product.amount !== undefined && (await this.setValue(this['Amount input'], product.amount));
+    product.notes && (await this.setValue(this['Notes textarea'], product.notes));
+  }
+
+  async fillInputsOverPrice(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    await this.setValue(this['Price input'], 1000000);
+    product.amount !== undefined && (await this.setValue(this['Amount input'], product.amount));
+    product.notes && (await this.setValue(this['Notes textarea'], product.notes));
+  }
+
+  async fillInputsLettersPrice(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    await this.setValue(this['Price input'], 'hundred');
+    product.amount !== undefined && (await this.setValue(this['Amount input'], product.amount));
+    product.notes && (await this.setValue(this['Notes textarea'], product.notes));
+  }
+
+  async fillInputsInavlidNotes(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    product.price !== undefined && (await this.setValue(this['Price input'], product.price));
+    product.amount !== undefined && (await this.setValue(this['Amount input'], product.amount));
+    product.notes && (await this.setValue(this['Notes textarea'], product.notes + '<A>'));
+  }
+
+  async fillInputs260Notes(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    product.price !== undefined && (await this.setValue(this['Price input'], product.price));
+    product.amount !== undefined && (await this.setValue(this['Amount input'], product.amount));
+    product.notes && (await this.setValue(this['Notes textarea'], product.notes.repeat(26)));
+  }
+
+  async fillInputsLetterAmount(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    product.price !== undefined && (await this.setValue(this['Price input'], product.price));
+    product.amount !== undefined && (await this.setValue(this['Amount input'], product.amount + 'l'));
+    product.notes && (await this.setValue(this['Notes textarea'], product.notes));
+  }
+
+  async fillInputsBigAmount(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    product.price !== undefined && (await this.setValue(this['Price input'], product.price));
+    await this.setValue(this['Amount input'], 10000);
+    product.notes && (await this.setValue(this['Notes textarea'], product.notes));
+  }
+
+  async fillInputsDecimalAmount(product: Partial<IProduct>) {
+    product.name && (await this.setValue(this['Name input'], product.name));
+    product.manufacturer && (await this.selectDropdownValue(this['Manufacturer dropdown'], product.manufacturer));
+    product.price !== undefined && (await this.setValue(this['Price input'], product.price));
+    await this.setValue(this['Amount input'], 5 / 2);
+    product.notes && (await this.setValue(this['Notes textarea'], product.notes));
+  }
+
+  async verifyNameErrorMessage() {
+    await $(this['Name Notification']).waitForDisplayed();
+  }
+
+  async verifyPriceErrorMessage() {
+    await $(this['Price Notification']).waitForDisplayed();
+  }
+
+  async verifyAmountErrorMessage() {
+    await $(this['Amount Notification']).waitForDisplayed();
+  }
+
+  async verifyNotesErrorMessage() {
+    await $(this['Notes Notification']).waitForDisplayed();
+  }
+
+  async verifyErrorMessage(field: 'Notes' | 'Name' | 'Price' | 'Amount') {
+    await $(this[`${field} Notification`]).waitForDisplayed();
+  }
+
+  async savebuttonDisabled() {
+    const saveButton = await $(this['Save New Product button']);
+    expect(await saveButton.isEnabled()).toBe(false);
   }
 
   async clickOnSaveButton() {
--- src/ui/services/products/addNewProduct.service.ts
+++ src/ui/services/products/addNewProduct.service.ts
@@ -14,6 +14,7 @@ export class AddProductService {
   async fillProductInputs(product: Partial<IProduct>) {
     await this.addNewProductPage.fillInputs(product);
   }
+
   @logStep('Save new product')
   async save() {
     await this.addNewProductPage.clickOnSaveButton();
@@ -25,4 +26,102 @@ export class AddProductService {
     await this.addNewProductPage.hiddenSpinner();
     await this.productsPage.waitForOpened();
   }
+
+  @logStep('Create product with short info in all fields')
+  async createShort(product?: IProduct) {
+    await this.fillProductInputsValidMinValues(product ?? generateNewProduct());
+    await this.save();
+    await this.addNewProductPage.hiddenSpinner();
+    await this.productsPage.waitForOpened();
+  }
+
+  @logStep('Create product with long info in all fields')
+  async createLong(product?: IProduct) {
+    await this.fillProductInputsValidMaxValues(product ?? generateNewProduct());
+    await this.save();
+    await this.addNewProductPage.hiddenSpinner();
+    await this.productsPage.waitForOpened();
+  }
+
+  @logStep('Fill product inputs with valid shortest values')
+  async fillProductInputsValidMinValues(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputsValidMinValues(product);
+  }
+
+  @logStep('Fill product inputs with valid longest values')
+  async fillProductInputsValidMaxValues(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputsValidMaxValues(product);
+  }
+
+  @logStep('Fill product inputs with invalid short name')
+  async fillProductInputsShortName(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputsShortName(product);
+  }
+
+  @logStep('Fill product inputs- Name with 2 spaces; negative')
+  async fillProductInputsSpacedName(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputs2SpacesName(product);
+  }
+
+  @logStep('Fill product inputs - 0 price')
+  async fillProductInputsZeroPrice(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputsZeroPrice(product);
+  }
+
+  @logStep('Fill product inputs - too big price')
+  async fillProductInputsHighPrice(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputsOverPrice(product);
+  }
+
+  @logStep('Fill product inputs price with letters')
+  async fillProductInputsLettersPrice(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputsLettersPrice(product);
+  }
+  @logStep('Fill product inputs - invalid notes')
+  async fillProductInputsInavlidNotes(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputsInavlidNotes(product);
+  }
+
+  @logStep('Fill product inputs - too long notes')
+  async fillProductInputsLongNotes(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputs260Notes(product);
+  }
+
+  @logStep('Fill product inputs -letters an amount')
+  async fillProductInputsLetterAmount(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputsLetterAmount(product);
+  }
+
+  @logStep('Fill product inputs - too big amount')
+  async fillProductInputsBigAmount(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputsBigAmount(product);
+  }
+
+  @logStep('Fill product inputs - decimall amount')
+  async fillProductInputsDecimalAmount(product: Partial<IProduct>) {
+    await this.addNewProductPage.fillInputsDecimalAmount(product);
+  }
+
+  @logStep('Verify notification is displayed')
+  async verifyErrorMessage(field: 'Notes' | 'Name' | 'Price' | 'Amount') {
+    await this.addNewProductPage.verifyErrorMessage(field);
+    await this.addNewProductPage.savebuttonDisabled();
+  }
+
+  @logStep('Verify save button is disabled')
+  async verifyBtnDisabled() {
+    await this.addNewProductPage.savebuttonDisabled();
+  }
 }
+// Добавьте возможность валидации формы создания продукта
+//   - Добавить методы для валидации валидационных сообщений в AddNewProductService и AddNewProductPage
+//   - Метод должен проверять наличие валидационной ошибки, ее текст, а также то, что кнопка Save New Product недоступна
+// Написать валидационные автотесты по созданию продукта согласно требованиям.
+// Позитивные:
+//   - Напишите минимум 2 позитивных кейса (с минимумом и максимумов разрешенных символов в инпутах)
+//   - В afterHook необходимо добавить шаги по удалению созданного продукта (нажать на карзину в таблице, удалить через модалку)
+
+// Негативные:
+//   - Негативные валидации по очереди для всех инпутов
+//   - В каждом тесте необходимо проверить валидационное сообщение под проверяем инпутом
+//   - В каждом тесте проверить, что Save New Product кнопка задизейблена
